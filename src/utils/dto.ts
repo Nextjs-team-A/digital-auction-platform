9f80f7eaf283dee5d0959b8869a31c25e79a1dea
royakais <kaisroya@gmail.com>
2025-11-30 11:51:59 +0200

    Added my work

--- src/utils/dto.ts
+++ src/utils/dto.ts
@@ -1,2 +1,33 @@
 // dto.ts
 // TODO: Shared DTO (Data Transfer Object) types or exports
+export interface RegisterDTO {
+  name: string;
+  email: string;
+  password: string;
+  role?:"SEEKER" | "PROVIDER" | "ADMIN" ; 
+}
+
+export interface LoginDTO {
+  email: string;
+  password: string;
+}
+
+export interface ProfileDTO {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface ChangePasswordDTO {
+  oldPassword: string;
+  newPassword: string;
+}
+
+export interface ResetPasswordDTO {
+  email: string;
+}
+
+export interface ForgotPasswordDTO{
+    token: string;
+    newPassword: string;
+}
